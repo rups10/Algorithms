e3ce0d0fd487072ed808d9986c90aba6356ffb8d
rupali <rupalikahalekar@gmail.com>
2022-09-26 18:14:31 +0530

    findPeak

--- Algo_Java/src/findPeakElement.java
+++ Algo_Java/src/findPeakElement.java
@@ -5,7 +5,18 @@
  * https://leetcode.com/problems/find-peak-element/
  */
 public class findPeakElement {
+    public static int search(int[] a, int l, int r) {
+        if(l==r)
+            return l;
+        
+        int mid = (l+r)/2;
+        
+        if(a[mid]>a[mid+1])
+            return search(a,l,mid);
+        return search(a,mid+1,r);
+    }
     
+    // approach with complexity 0(n)
     public static int findPeakElement(int[] a) {
         int n = a.length;
         int i=0;
@@ -24,10 +35,10 @@ public class findPeakElement {
     
     public static void main(String[] args) {
         // TODO code application logic here
-        int[] a = {1,2,3,6};
+        int[] a = {1,2,3,6,4,0};
         
-        int result = findPeakElement(a);
-        
+        //int result = findPeakElement(a);
+        int result = search(a,0,a.length-1);
         System.out.println("peak element " + result);
     }
 }
